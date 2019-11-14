ea27b847f70fad3f12e0dcf74326f7496eecee15
Brady Gaster <bradygaster@Bradys-MacBook-Pro.local>
2019-11-14 10:56:48 -0800

    working e2e

--- AadAuthenticatedApi/Startup.cs
+++ AadAuthenticatedApi/Startup.cs
@@ -1,21 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.Identity.Web;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web.Resource;
 
 namespace AadAuthenticatedApi
 {
@@ -31,44 +20,7 @@ namespace AadAuthenticatedApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.JwtBearerAuthenticationScheme)
-                    .AddAzureADBearer(options => Configuration.Bind("AzureAD", options));
-            services.Configure<AzureADOptions>(options => Configuration.Bind("AzureAD", options));
-
-            services.AddHttpContextAccessor();
-
-            services.Configure<JwtBearerOptions>(AzureADDefaults.JwtBearerAuthenticationScheme, options =>
-            {
-                // This is an Microsoft identity platform Web API
-                options.Authority += "/v2.0";
-
-                // The valid audiences are both the Client ID (options.Audience) and api://{ClientID}
-                options.TokenValidationParameters.ValidAudiences = new string[]
-                {
-                    options.Audience, $"api://{options.Audience}"
-                };
-
-                // Instead of using the default validation (validating against a single tenant, as we do in line of business apps),
-                // we inject our own multi-tenant validation logic (which even accepts both v1.0 and v2.0 tokens)
-                options.TokenValidationParameters.IssuerValidator = AadIssuerValidator.GetIssuerValidator(options.Authority).Validate;
-
-                // When an access token for our own Web API is validated, we add it to MSAL.NET's cache so that it can
-                // be used from the controllers.
-                options.Events = new JwtBearerEvents();
-
-                options.Events.OnTokenValidated = async context =>
-                   {
-                       // This check is required to ensure that the Web API only accepts tokens from tenants where it has been consented and provisioned.
-                       if (!context.Principal.Claims.Any(x => x.Type == ClaimConstants.Scope)
-                        && !context.Principal.Claims.Any(y => y.Type == ClaimConstants.Scp)
-                        && !context.Principal.Claims.Any(y => y.Type == ClaimConstants.Roles))
-                       {
-                           throw new UnauthorizedAccessException("Neither scope or roles claim was found in the bearer token.");
-                       }
-
-                       await Task.FromResult(0);
-                   };
-            });
+            services.AddProtectedWebApi(Configuration);
 
             services.AddControllers();
         }
